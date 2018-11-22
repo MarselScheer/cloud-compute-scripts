1518b5b9c180a91e7586c16f5cb06839d54dd340
Marsel Scheer <scheer@freescience.de>
2018-11-22 17:51:32 +0000

    additional R-libs

--- libs.R
+++ libs.R
@@ -1,6 +1,8 @@
 install.packages(
-    c('drake', 'data.table', 'tictoc', 'glue', 'dplyr', 'purrr', 'tidyr', 'stringr', 'readr', 'ggplot2',
-      'simTool', 'caret', 'car', 'furrr', 'testthat', 'funModeling', 'klaR', 'rBayesianOptimization',
-      'SmartEDA', 'rmarkdown', 'knitr', 'DT', 'skimr', 'xgboost'),
+    c('car', 'caret', 'corrplot', 'corrr', 'data.table', 'dplyr', 'drake', 'DT', 'embed',
+    'funModeling', 'furrr', 'futile.logger', 'ggplot2', 'glue', 'klaR', 'knitr', 'partykit', 'purrr',
+    'rBayesianOptimization', 'readr', 'recipes', 'rmarkdown', 'rsample', 'simTool',
+    'skimr', 'SmartEDA', 'stringr', 'testthat', 'tictoc', 'tidyr', 'vtreat', 'xgboost'),
     repos = 'https://ftp.gwdg.de/pub/misc/cran/',
-    Ncpus = 8)
+    Ncpus = 8
+)
