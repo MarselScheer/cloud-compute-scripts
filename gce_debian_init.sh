d99400bb1a76e27e1b98542af8e2d084643eae6a
m <scheer@freescience.de>
2018-12-08 18:16:51 +0000

    git-name and email in a separate file

--- gce_debian_init.sh
+++ gce_debian_init.sh
@@ -3,8 +3,7 @@
 # Prepares an *Debian 9 + TF1-9* image on google cloud platform for R
 
 export LANG=en_US.UTF-8
-GIT_NAME="m"
-GIT_EMAIL="scheer@freescience.de"
+
 
 sudo ln -sf /usr/share/zoneinfo/GMT /etc/localtime
 
@@ -18,12 +17,19 @@ sudo apt-get install dirmngr libcurl4-openssl-dev libssl-dev
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
 sudo apt-get update
 sudo apt-get install -y r-base r-base-dev
-git config --global user.name $GIT_NAME
-git config --global user.email $GIT_EMAIL
 
 sudo apt-get install -y htop ranger python-pip git tig emacs-nox pandoc ed
 echo "export EDITOR=emacs" >> ~/.bashrc
 
 sudo Rscript ./libs.R
 
+source ./GIT_CONFIG
+echo "Set git user and email to:"
+echo $GIT_NAME
+echo $GIT_EMAIL
+git config --global user.name $GIT_NAME
+git config --global user.email $GIT_EMAIL
+
+
 echo "execute: source ~/.bashrc"
+
