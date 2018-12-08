0b694e046acae4979821c25b06c2fa1653cb601d
Marsel Scheer <scheer@freescience.com>
2018-12-08 14:42:00 +0000

    install only packages that does exist already

--- libs.R
+++ libs.R
@@ -1,10 +1,20 @@
-install.packages('pacman', repos = 'https://ftp.gwdg.de/pub/misc/cran/')
+if (!require(pacman)) {
+  install.packages('pacman', repos = 'https://ftp.gwdg.de/pub/misc/cran/')
+}
 
-pacman::p_install(
-    c('car', 'caret', 'corrplot', 'corrr', 'data.table', 'dplyr', 'drake', 'DT', 'embed',
+cran_pkg <- c('car', 'caret', 'corrplot', 'corrr', 'data.table', 'dplyr', 'drake', 'DT', 'embed',
     'funModeling', 'furrr', 'futile.logger', 'ggplot2', 'glue', 'klaR', 'knitr', 'partykit', 'purrr',
     'rBayesianOptimization', 'readr', 'recipes', 'rmarkdown', 'rsample', 'simTool',
-    'skimr', 'SmartEDA', 'stringr', 'testthat', 'tictoc', 'tidyr', 'vtreat', 'xgboost'),
+    'skimr', 'SmartEDA', 'stringr', 'testthat', 'tictoc', 'tidyr', 'vtreat', 'xgboost')
+
+pkg_installed <- sapply(cran_pkg, pacman::p_exists, local = TRUE)
+
+if (any(pkg_installed)) {
+   pacman::p_install(
+    cran_pkg[ !pkg_installed],
+    character.only = TRUE,
     repos = 'https://ftp.gwdg.de/pub/misc/cran/',
     Ncpus = 8
-)
+   )
+}
+
