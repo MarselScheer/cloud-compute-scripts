365ed6372ddd9020d9d3b60b97d641c3e09385c3
Marsel Scheer <scheer@freescience.com>
2018-12-08 14:54:04 +0000

    additional r-libs

--- libs.R
+++ libs.R
@@ -3,13 +3,13 @@ if (!require(pacman)) {
 }
 
 cran_pkg <- c('car', 'caret', 'corrplot', 'corrr', 'data.table', 'dplyr', 'drake', 'DT', 'embed',
-    'funModeling', 'furrr', 'futile.logger', 'ggplot2', 'glue', 'klaR', 'knitr', 'partykit', 'purrr',
+    'funModeling', 'furrr', 'futile.logger', 'ggplot2', 'glue', 'klaR', 'knitr', 'MLmetrics', 'partykit', 'purrr',
     'rBayesianOptimization', 'readr', 'recipes', 'rmarkdown', 'rsample', 'simTool',
     'skimr', 'SmartEDA', 'stringr', 'testthat', 'tictoc', 'tidyr', 'vtreat', 'visNetwork', 'xgboost')
 
 pkg_installed <- sapply(cran_pkg, pacman::p_exists, local = TRUE)
 
-if (any(pkg_installed)) {
+if (any(!pkg_installed)) {
    pacman::p_install(
     cran_pkg[ !pkg_installed],
     character.only = TRUE,
