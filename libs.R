240d1ab1ebeee65b01e7c72a2b7e47935057225a
Marsel Scheer <scheer@freescience.de>
2018-12-08 12:08:30 +0000

    install r-libs with pacman

--- libs.R
+++ libs.R
@@ -1,4 +1,6 @@
-install.packages(
+install.packages('pacman', repos = 'https://ftp.gwdg.de/pub/misc/cran/')
+
+pacman::p_install(
     c('car', 'caret', 'corrplot', 'corrr', 'data.table', 'dplyr', 'drake', 'DT', 'embed',
     'funModeling', 'furrr', 'futile.logger', 'ggplot2', 'glue', 'klaR', 'knitr', 'partykit', 'purrr',
     'rBayesianOptimization', 'readr', 'recipes', 'rmarkdown', 'rsample', 'simTool',
